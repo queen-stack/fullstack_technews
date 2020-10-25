82b50a4880c38ec95d8fe70a10f69a5c9d0db1f2
queen-stack <67836023+queen-stack@users.noreply.github.com>
2020-10-25 14:22:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
+onst path = require('path');
 const express = require('express');
-const session = require("express-session");
-const hbs = require('express-handlebars'); 
-const sequelize = require('./config/connection')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const routes = require('./controllers');
-const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const handlebarsHelpers = require('./utils/helpers');
-const exphbs = hbs({helpers: handlebarsHelpers});
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {
-    //Session to expire in approx 10 min.  
-    maxAge: 10 * 60 * 1000
-  
-  },
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -30,11 +20,20 @@ const sess = {
 };
 
 app.use(session(sess));
-app.use(routes);
-app.engine('handlebars', exphbs);
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// turn on connection to db and server
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
